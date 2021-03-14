eec5096386cc3a1741c83364e1db5b7ee87b84e1
meyadavajeet <meyadavajeet@gmail.com>
2021-03-14 08:00:18 +0530

    Read data from mongodb database in nodejs with express without api

--- express/expressWithMongo/app.js
+++ express/expressWithMongo/app.js
@@ -1,8 +1,26 @@
+//for connection
+// const express = require('express');
+// const app = express();
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb+srv://nodeuser:nodeuser@123@cluster0.znxrx.mongodb.net/test?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => console.warn("Database Test Connected with mongoDb"))
+//     .catch((error) => console.warn(error));
+//end of connection
+
+
+// get data from the mongodb
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const userModel = require('./models/users');
 mongoose.connect("mongodb+srv://nodeuser:nodeuser@123@cluster0.znxrx.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.warn("Database Test Connected with mongoDb"))
-    .catch((error) => console.warn(error));
+});
+
+userModel.find({},(err,user)=>{
+    if(err) console.warn(err);
+    console.warn(user);
+});
