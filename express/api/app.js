bf75d516d1ca9668d09fd4e9a8e23c74d0ab2a42
meyadavajeet <meyadavajeet@gmail.com>
2021-03-15 00:13:14 +0530

    after get and post api of user

---
+++ express/api/app.js
@@ -0,0 +1,38 @@
+// get data from the mongodb
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+const userModel = require('./models/User');
+mongoose.connect("mongodb+srv://nodeuser:nodeuser@123@cluster0.znxrx.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.get('/users', (req, res) => {
+    userModel.find().then((data) => {
+        res.status(200).json(data);
+    }).catch((error) => console.warn(error));;
+    // userModel.find().select('name').then((data)=>{
+    //     res.status(200).json(data);
+    // });
+});
+
+//end of get data from mongodb
+
+//post data to mongo db using postman
+//post api using express
+app.post('/user', jsonParser, (req, res) => {
+    const data = new userModel({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        password: req.body.password
+    })
+    data.save().then((result) => res.status(201).json(result))
+        .catch((error) => console.warn(error));
+})
+
+app.listen(4000);
