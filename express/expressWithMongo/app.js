69707bd1386276db1e5e533265edd19d60ad05b6
meyadavajeet <meyadavajeet@gmail.com>
2021-03-14 08:14:14 +0530

    after insert data in mongodb collection with node express without api

--- express/expressWithMongo/app.js
+++ express/expressWithMongo/app.js
@@ -11,6 +11,24 @@
 
 
 // get data from the mongodb
+// const express = require('express');
+// const app = express();
+// const mongoose = require("mongoose");
+// const userModel = require('./models/users');
+// mongoose.connect("mongodb+srv://nodeuser:nodeuser@123@cluster0.znxrx.mongodb.net/test?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// userModel.find({},(err,user)=>{
+//     if(err) console.warn(err);
+//     console.warn(user);
+// });
+
+//end of get data from mongodb
+
+
+//save data to mongo db
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
@@ -20,7 +38,12 @@ mongoose.connect("mongodb+srv://nodeuser:nodeuser@123@cluster0.znxrx.mongodb.net
     useUnifiedTopology: true
 });
 
-userModel.find({},(err,user)=>{
-    if(err) console.warn(err);
-    console.warn(user);
+const data = new userModel({
+    _id: new mongoose.Types.ObjectId(),
+    name: "bruce",
+    email: "banner@gmail.com",
+    address: "USA"
 });
+
+data.save().then((result) => console.warn(result)).catch((error) => console.log(error));
+// end of save data to mongodb
