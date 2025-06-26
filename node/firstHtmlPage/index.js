354e1cd4ad5578895f5eb11033006db07420e57a
Ajeet Yadav <meyadavajeet@gmail.com>
2025-06-26 07:59:17 +0530

    html page

--- node/firstHtmlPage/index.js
+++ node/firstHtmlPage/index.js
@@ -1,13 +1,25 @@
 var http = require("http");
-var page =
-    `<h1>First Html Page using node js</h1><br><br>
+const fs = require("node:fs");
+var page = `<h1>First Html Page using node js</h1><br><br>
     <input type="text" /><br><br>
     <input type="text" /><br><br>
     <input type="text" /><br><br>
     <input type="text" /><br><br>
     `;
-http.createServer(function(req, res){
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.write(page);
-    res.end();
-}).listen(9000);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    // const html = fs.readFileSync("./index.html", "utf-8");
+    // // res.write(page);
+    // res.write(html);
+    // res.end();
+
+    //do same with the help of stream
+    // fs.createReadStream("./index.html").pipe(res);
+
+    // dynamic value
+    let html = fs.readFileSync("./index.html", "utf-8");
+    html = html.replace("{{name}}", "Ajeet");
+    res.end(html);
+  })
+  .listen(9000, () => console.log("server listen on 9000"));
